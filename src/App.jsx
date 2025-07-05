16ad63daba46292c8d8408437cbaa90ccfb36c12
UPPALURI <ruppalur@7cf34dd688a9.ant.amazon.com>
2025-07-05 19:43:12 +1000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+        <h1>Hello Ram ! </h1>
+        <h2>Welcome to AWS & React world</h2>
+      </header>
+    </div>
+  );
+}
+export default App;
